0a8eb4c1584b6b0a333945658b2ff8be39280d73
Andrew Monis <5andrewmonis7@gmail.com>
2022-01-13 19:44:12 -0500

    fixed a bug with the search field

--- src/components/search.js
+++ src/components/search.js
@@ -2,15 +2,15 @@ import React, { useState } from 'react'
 
 const Search = () => {
     const [ search, setSearch ] = useState("")
-    function handleChange(event) {
-        if(event.key === "Enter"){
-            setSearch("")
-        }
+    
+    function clear(){
+        setSearch("")
     }
+
     return (
         <div>
             <form action="https://www.google.com/search" method="get" name="searchform" target="_blank">
-                <input onChange={event => setSearch(event.target.value)} autoComplete="off" name="q" className="search" type="text" placeholder="Search..." value={search} onKeyPress={handleChange} />
+                <input onChange={event => setSearch(event.target.value)} autoComplete="off" name="q" className="search" type="text" placeholder="Search..." value={search} onClick={clear}  />
             </form>
         </div>
     )
