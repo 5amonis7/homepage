2c6133aea1a25f61e368d8f7f3accddd9b58176a
Andrew Monis <5andrewmonis7@gmail.com>
2022-01-18 11:43:07 -0500

    changed icon to figma

--- src/components/nav/work.js
+++ src/components/nav/work.js
@@ -5,7 +5,7 @@ const work = () => {
     const linkedin = { url: 'https://www.linkedin.com/in/andrew-monis-022681209/'}
     const github = { url: 'https://github.com/5amonis7'}
     const stack = { url: 'https://stackoverflow.com/users/16513794/andrew-monis'}
-    const udemy = { url: 'https://www.udemy.com/'}
+    const figma = { url: 'https://www.figma.com/'}
 
     return (
         <div className='work'>
@@ -14,7 +14,7 @@ const work = () => {
                 <a rel="noreferrer" target="_blank" href={linkedin.url} class='linkedin' ><img src={require('./images/linkedin.png')} alt="linkedin" /></a>
                 <a rel="noreferrer" target="_blank" href={github.url} class='github' ><img src={require('./images/github1.png')} alt="github" /></a>
                 <a rel="noreferrer" target="_blank" href={stack.url} class='stack' ><img src={require('./images/stack.png')} alt="stack" /></a>
-                <a rel="noreferrer" target="_blank" href={udemy.url} class='udemy' ><img src={require('./images/udemy.png')} alt="udemy" /></a>
+                <a rel="noreferrer" target="_blank" href={figma.url} class='figma' ><img src={require('./images/figma.png')} alt="figma" /></a>
             </div>
         </div>
     )
